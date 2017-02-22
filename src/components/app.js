0f8560045fc4c91ca2f4a1fd45266ee21b463134
Jeremy Walla <jwalla1@jccc.edu>
2017-02-22 02:56:26 +0000

    Initial commit

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import Header from './Header'
 
 export default React.createClass({
   render: function () {
     return (
       <div>
-        Welcome to React!
+        <Header />
+        
+        Welcome to Foobar!
       </div>
     );
   }
